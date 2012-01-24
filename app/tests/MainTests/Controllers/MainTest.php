c486256a7029a6aba28f14c633bee3c287dc3790
Student Ivan <cryptox3@gmail.com>
2012-01-24 17:59:54 +0400

    markdown

--- app/tests/MainTests/Controllers/MainTest.php
+++ app/tests/MainTests/Controllers/MainTest.php
@@ -14,22 +14,9 @@ class MainTest extends \PHPUnit_Framework_TestCase {
 	}
 	
 	public function test() {
-        /*$_POST['myarray'] = array(1, 2, 3);
-        $_POST['myinteger'] = 100;
-        $_POST['mystring'] = 'test';
-        $_POST['mybool'] = false;
-        $_POST['mynull'] = null;*/
         $request = WebRequest::createFromGlobals();
         $response = new WebResponse();
-        //$defender = new \Framework\Common\Defender();
-        //var_dump($defender->needArray($request->post('mynull')));
-        /*var_dump($request->postArray('myarray'));
-        var_dump($request->postInt('myinteger'));
-        var_dump($request->postStr('mystring'));
-        var_dump($request->postArray('myinteger'));
-        var_dump($request->postInt('mystring'));
-        var_dump($request->postStr('myarray'));*/
-		ob_start();
+        ob_start();
         $this->component->index($request, $response);
         $content = ob_get_clean();
         $this->assertEquals('eeeee', $content);
