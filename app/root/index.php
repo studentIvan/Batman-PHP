323d8caa73360c6aba84cc0e200f2e0ef03bc977
Student Ivan <cryptox3@gmail.com>
2012-01-25 16:00:51 +0400

    frontfix

--- app/root/index.php
+++ app/root/index.php
@@ -6,5 +6,4 @@ error_reporting(-1);
 ini_set('display_errors', 'On');
 chdir('../..');
 require_once 'autoload.php';
-$defaultBundle = Framework\Core\Config::get('application', 'default');
 Bootstrap\Bootstrap::boot();
