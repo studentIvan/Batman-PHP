88de4693d85c93fc8a42bcf3442d6e0cb2348006
Student Ivan <cryptox3@gmail.com>
2012-03-02 00:46:31 +0400

    cosmetic

--- system/Framework/Core/Terminator.php
+++ system/Framework/Core/Terminator.php
@@ -3,18 +3,10 @@ namespace Framework\Core;
 use \Exceptions\NotFoundException;
 use \ErrorException;
  
-class Terminator {
-
-    /**
-     * @static
-     * @throws \ErrorException
-     * @param int $errno
-     * @param string $errstr
-     * @param string $errfile
-     * @param int $errline
-     * @return void
-     */
-    public static function terminate($errno, $errstr, $errfile, $errline) {
+class Terminator 
+{
+    public static function terminate($errno, $errstr, $errfile, $errline) 
+    {
         if ($errno == 4096 and (strpos($errstr, 'WebRequest, string given') !== false)) {
             throw new NotFoundException();
         } else {
