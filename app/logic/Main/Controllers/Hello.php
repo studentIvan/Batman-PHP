3caebbf588cd862c8fc16cf358f1eeb7e666c99c
Student Ivan <cryptox3@gmail.com>
2012-03-11 18:27:46 +0400

    replace response on return in default hello example

--- app/logic/Main/Controllers/Hello.php
+++ app/logic/Main/Controllers/Hello.php
@@ -1,14 +1,11 @@
 <?php
 namespace Main\Controllers;
 
-use \Framework\Common\WebRequest,
-    \Framework\Common\WebResponse;
-
 class Hello extends \Framework\Core\Controller
 {
-    public function index($name, WebResponse $response)
+    public function index($name)
     {
         $this->tpl->match('name', $name);
-        $response->send($this->tpl->render('hello'));
+        return $this->tpl->render('hello');
     }
 }
--- app/tests/MainTests/Controllers/HelloTest.php
+++ app/tests/MainTests/Controllers/HelloTest.php
@@ -1,16 +1,13 @@
 <?php
 namespace MainTests\Controllers;
 
-use \Framework\Common\WebRequest,
-    \Framework\Common\WebResponse,
-    \Framework\Core\Template,
+use \Framework\Core\Template,
     \Main\Controllers\Hello;
 
 require_once 'PHPUnit/Autoload.php';
 
 class HelloTest extends \PHPUnit_Framework_TestCase
 {
-
     public function __construct()
     {
         $this->component = new Hello();
@@ -19,14 +16,6 @@ class HelloTest extends \PHPUnit_Framework_TestCase
     
     public function test()
     {
-        $request = new WebRequest();
-        $response = new WebResponse();
-
-        ob_start();
-        $this->component->index('World', $response, $request);
-        $content = ob_get_clean();
-
-        $this->assertRegExp('/Hello <b>World<\/b>/', $content);
+        $this->assertRegExp('/Hello <b>World<\/b>/', $this->component->index('World'));
     }
-
 }
